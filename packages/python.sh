41e28126990eb3eb01db0059225584748bf2b2dc
Connor Bray <connor.bray@icloud.com>
2017-04-17 12:54:29 +0100

    Updated pip install to upgrade pip

--- packages/python.sh
+++ packages/python.sh
@@ -5,4 +5,5 @@ echo "Installing Python"
 echo "========================================================================"
 
 sudo apt-get -y install python python3 python-pip python3-dev python3-pip
-sudo -H pip3 install thefuck
+sudo -H pip install --upgrade pip
+sudo -H pip install thefuck
